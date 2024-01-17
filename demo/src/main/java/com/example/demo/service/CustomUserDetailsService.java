80a7d9acbb782b46bcbc8809d6c598bab1cc86f7
menj1001 <menj1001@stud.hs-kl.de>
2024-01-17 16:46:50 +0100

    Arranged imports

--- demo/src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ demo/src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.User;
@@ -12,9 +11,9 @@ import com.example.demo.repositories.UserRepository;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-	
-	 @Autowired
-	 private UserRepository userRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -22,7 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 		if (user == null) {
 			throw new UsernameNotFoundException("user not found");
 		}
-		
+
 		return new CustomUserDetail(user);
 
 	}
