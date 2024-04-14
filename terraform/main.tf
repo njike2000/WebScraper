4933308049288e046818b934e554fd4f55250010
menj1001 <menj1001@stud.hs-kl.de>
2024-04-14 22:24:26 +0200

    update terraform k8s.ft and outputs.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,12 @@ data "google_client_config" "current" {}
 resource "google_container_cluster" "default" {
   name               = "my-first-cluster"
   location           = "europe-west3"
-  initial_node_count = 3
+  initial_node_count = 6
   min_master_version = data.google_container_engine_versions.default.latest_master_version
 
   node_config {
     machine_type = "g1-small"
-    disk_size_gb = 32
+    disk_size_gb = 64
   }
 
   provisioner "local-exec" {
