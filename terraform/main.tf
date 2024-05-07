22e2a46ce98f63bcb752c23579552176bdad5a60
menj1001 <menj1001@stud.hs-kl.de>
2024-05-07 12:18:24 +0200

    updated terraform main.tf: set cluste auto delete to false

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,19 @@
+# Define data sources to fetch required information
 data "google_container_engine_versions" "default" {
   location = "europe-west3"
 }
 
 data "google_client_config" "current" {}
 
+# Define the GKE cluster resource
 resource "google_container_cluster" "default" {
   name               = "my-first-cluster"
   location           = "europe-west3"
   initial_node_count = 3
   min_master_version = data.google_container_engine_versions.default.latest_master_version
 
-  deletion_protection = false  # Set deletion_protection to false
+  # Ensure deletion_protection is set to false
+  deletion_protection = false
 
   node_config {
     machine_type = "g1-small"
@@ -23,10 +26,11 @@ resource "google_container_cluster" "default" {
   }
 }
 
-#  lifecycle {
- #   ignore_changes = [
- #     node_config[0].machine_type,
- #   ]
- # }
+# Define the Kubernetes deployment resource
+resource "kubernetes_deployment" "name" {
+  # Define the deployment configuration...
+}
 
+# Define the workflow for deploying the infrastructure
+# This could include steps such as authentication, Docker image build, Terraform initialization, plan, and apply.
 
