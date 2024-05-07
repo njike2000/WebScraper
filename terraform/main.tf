9c9a2cbbf059c6b272203660c33135e696735904
menj1001 <menj1001@stud.hs-kl.de>
2024-05-07 12:34:11 +0200

    updated terraform main.tf: set cluste auto delete to false

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "google_container_cluster" "default" {
   initial_node_count = 3
   min_master_version = data.google_container_engine_versions.default.latest_master_version
 
-  # Ensure deletion_protection is set to false
+  # Set deletion_protection to false
   deletion_protection = false
 
   node_config {
@@ -25,6 +25,9 @@ resource "google_container_cluster" "default" {
     command = "sleep 90"
   }
 }
+
+# Define the workflow for deploying the infrastructure
+# This could include steps such as authentication, Docker image build, Terraform initialization, plan, and apply.
 
 
 # Define the workflow for deploying the infrastructure
